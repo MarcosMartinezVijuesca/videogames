9eef5b71cff1082b38c05d76f64b642849535444
Marcos Martínez <martinezvijuescamarcos@gmail.com>
2025-01-14 17:47:38 +0100

    Se han añadido mejoras al frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,39 @@
 import axios from 'axios';
-import { element } from './documentUtil.js';
+import { element, icon, td } from './documentUtil.js';
+import { notifySuccess } from './warningUtil.js';
+
+
 
 window.readVideogames = function() {
     axios.get('http://localhost:8080/videogames')
         .then((response) => {
             const videogameList = response.data;
-            const videogameUl = element('videogames');
+            const videogameTable = element('tableBody');
 
             videogameList.forEach(videogame => {
-                const listItem = document.createElement('li');
-                listItem.className = 'list-group-item';
-                listItem.appendChild(document.createTextNode(videogame.name + ' ' + '|' + ' ' + videogame.type + ' ' + '|' + ' ' + videogame.year));
-                videogameUl.appendChild(listItem);
+                const row =  document.createElement('tr');
+                row.id = 'videogame-' + videogame.id;
+                row.innerHTML = td(videogame.name) + 
+                                td(videogame.type) + 
+                                td (videogame.year) + 
+                                '<a class="btn btn-info" href="modify.html">' +
+                                icon('edit') +
+                                '</a> ' +  '<a class="btn btn-danger" href="javascript:removeVideogame('+ videogame.id + ')">' + 
+                                icon('delete') +
+                                '</a>';
+                videogameTable.appendChild(row);
             })
         });
-}
+};
+
+window.removeVideogame = function(id) {
+    if(confirm('¿Seguro que deseas eliminar este videogame?')){
+    axios.delete('http://localhost:8080/videogames/' + id)
+        .then((response) => {
+            if (response.status == 204){
+                notifySuccess('Videogame eliminado satisfactoriamente.');
+                element('videogame-' + id).remove();
+            }
+        });
+    }
+};
