c082ac0f9d57e9ab7709446f0f4e599b9afc6283
MarcosMartinezVijuesca <martinezvijuescamarcos@gmail.com>
2025-05-24 17:52:20 +0200

    mocked unit tests from controller

--- backend/src/test/unit/unit.test.js
+++ backend/src/test/unit/unit.test.js
@@ -1,46 +1,104 @@
-const expect = require('chai').expect;
-const knex = require('knex');
-const db = knex({
-    client: 'sqlite3',
-    connection: {
-        filename: 'videogames.db'
-    },
-    useNullAsDefault: true
+const httpMocks = require('node-mocks-http');
+const { describe, it, expect, afterEach } = require('@jest/globals');
+
+jest.mock('../../service/videogames');
+
+const videogameController = require('../../controller/videogames');
+
+const videogameService = require('../../service/videogames');
+const mockedFindVideogames = jest.spyOn(videogameService, 'findVideogames');
+const mockedRegisterVideogame = jest.spyOn(videogameService, "registerVideogame");
+const { mockVideogameArray, mockVideogameToPost, mockVideogameResponse, mockVideogameToRegister } = require('./mocks/videogames');
+
+afterEach(() => {
+    jest.clearAllMocks();
 });
 
+describe('videogames', () => {
+    it('GET /videogames should get a videogame list', async () => {
+        const response = httpMocks.createResponse();
+        const request = httpMocks.createRequest();
+        request.app = {};
+        request.app.conf = {};
+        request.path = '/videogames';
 
-    // Unit tests for videogame data fields
-    describe('videogame data fields', () => {
-        it('should validate videogame name is a non-empty string', () => {
-            const videogame = { name: 'Skyrim' };
-            expect(videogame.name).to.be.a('string');
-            expect(videogame.name.length).to.be.greaterThan(0);
+        const mockedVideogameList = jest.fn(async () => {
+            return mockVideogameArray;
         });
+        mockedFindVideogames.mockImplementation(mockedVideogameList);
 
-        it('should validate videogame type is a valid string', () => {
-            const videogame = { type: 'Adventure' };
-            expect(videogame.type).to.be.a('string');
-            expect(['Adventure', 'Action', 'RPG', 'Sports', 'Puzzle']).to.include(videogame.type);
-        });
-
-        it('should validate videogame year is a valid number', () => {
-            const videogame = { year: 2020 };
-            expect(videogame.year).to.be.a('number');
-            expect(videogame.year).to.be.within(1970, new Date().getFullYear());
-        });
+        await videogameController.getVideogames(request, response);
+        expect(mockedFindVideogames).toHaveBeenCalledTimes(1);
+        expect(response.statusCode).toEqual(200);
+        expect(response._isEndCalled()).toBeTruthy();
+        expect(response._getJSONData().length).toEqual(10);
     });
 
-    // Unit tests for videogame data retrieval
-    describe('videogame data retrieval', () => {
-        it('should retrieve a videogame by name', async () => {
-            const name = 'Skyrim';
-            const response = await db('videogames').select('*').where({ name }).first();
-            expect(response).to.have.property('name', name);
-        });  
-        afterAll(async () => {
-    await db.destroy();
+    it('POST /videogames should register a new videogame', async () => {
+        const response = httpMocks.createResponse();
+        const request = httpMocks.createRequest();
+        request.app = {};
+        request.app.conf = {};
+        request.path = '/videogames';
+        request.body = mockVideogameToRegister;
+
+        const mockedRegisterVideogameResponse = jest.fn(async () => {
+            return mockVideogameResponse;
+        });
+        mockedRegisterVideogame.mockImplementation(mockedRegisterVideogameResponse);
+
+        await videogameController.postVideogame(request, response);
+        expect(mockedRegisterVideogame).toHaveBeenCalledTimes(1);
+        expect(response.statusCode).toEqual(201);
+        expect(response._isEndCalled()).toBeTruthy();
+        expect(response._getJSONData().id).toEqual(1);
+        expect(response._getJSONData().name).toEqual('Elden Ring');
+        expect(response._getJSONData().type).toEqual('souls');
+        expect(response._getJSONData().year).toEqual(2021);
+
+    });
+
+    it('PUT /videogames/:videogameId should update a videogame', async () => {
+        const response = httpMocks.createResponse();
+        const request = httpMocks.createRequest();
+        request.app = {};
+        request.app.conf = {};
+        request.path = '/videogames/1';
+        request.params = { videogameId: 1 };
+        request.body = {
+            name: 'Updated Game',
+            type: 'AUTO',
+            year: 2023
+        };
+
+        const mockedModifyVideogameResponse = jest.fn(async () => {
+            return true;
+        });
+        jest.spyOn(videogameService, 'modifyVideogame').mockImplementation(mockedModifyVideogameResponse);
+
+        await videogameController.putVideogame(request, response);
+        expect(videogameService.modifyVideogame).toHaveBeenCalledTimes(1);
+        expect(response.statusCode).toEqual(200);
+        expect(response._isEndCalled()).toBeTruthy();
+    });
+
+    it('DELETE /videogames/:videogameId should delete a videogame', async () => {
+        const response = httpMocks.createResponse();
+        const request = httpMocks.createRequest();
+        request.app = {};
+        request.app.conf = {};
+        request.path = '/videogames/1';
+        request.params = { videogameId: 1 };
+
+        const mockedDeleteVideogameResponse = jest.fn(async () => {
+            return true;
+        });
+        jest.spyOn(videogameService, 'removeVideogame').mockImplementation(mockedDeleteVideogameResponse);
+
+        await videogameController.deleteVideogame(request, response);
+        expect(videogameService.removeVideogame).toHaveBeenCalledTimes(1);
+        expect(response.statusCode).toEqual(200);
+        expect(response._isEndCalled()).toBeTruthy();
+    });
+
 });
-    });
-
-      
-    
